38f205dfae14a9c2fca39cd560f07876627564f2
Mirasaki <pcr.hillebrand1998@gmail.com>
2024-11-07 18:51:15 +0100

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const CFTOOLS_API_BASE_URL = 'https://data.cftools.cloud';
+export const CFTOOLS_V1_API_BASE_URL = `${CFTOOLS_API_BASE_URL}/v1`;
+export const CFTOOLS_V2_API_BASE_URL = `${CFTOOLS_API_BASE_URL}/v2`;
